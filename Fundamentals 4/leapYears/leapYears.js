5a7c96be87fd1d6ce7d0f18bd0d94b0afd603706
safeplace12345 <safeplace12345@gmail.com>
2020-11-19 17:04:59 +0200

    Solving the leapYears challenge

--- Fundamentals 4/leapYears/leapYears.js
+++ Fundamentals 4/leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return true ? ((year % 400 === 0) || (year % 100 !== 0)) && ((year % 4) == 0) : false;
 }
 
 module.exports = leapYears
