c2643fa40d5576bd70e5b25cb5047bbb1c1ad7d3
safeplace12345 <safeplace12345@gmail.com>
2020-11-21 13:45:45 +0200

    still stuck on the repeats the string a random amount of times

--- Fundamentals 4/repeatString/repeatString.js
+++ Fundamentals 4/repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str) {
+    let repeat = ''
+   
+ for(let i = 0 ; i < Math.floor(Math.random() * str.length) ; i++){
+  repeat += str
+ }
+ return repeat;
 }
 
+
+console.log(repeatString('hey'))
 module.exports = repeatString
