de49654ef2c72385cfb2f7404b04514a058e50fe
safeplace12345 <safeplace12345@gmail.com>
2020-12-15 17:42:02 +0200

    find the oldest solved

--- Fundamentals 5/fibonacci/fibonacci.js
+++ Fundamentals 5/fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let start,end,i,result;
+  start = 0
+  end = 1
 
+  for(i = 2 ; i < num ; i++){
+    //   console.log(i)
+   result = start + end;
+   start = end;
+   end = result;
+   console.log(result)
+  }
+  return result;
 }
-
-module.exports = fibonacci
+console.log(fibonacci(6))
+// module.exports = fibonacci
--- Fundamentals 5/findTheOldest/findTheOldest.js
+++ Fundamentals 5/findTheOldest/findTheOldest.js
@@ -1,5 +1,98 @@
-let findTheOldest = function() {
+// const people = [{
+//         name: 'Carly',
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//     },
+//     {
+//         name: 'Ray',
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011
+//     },
+//     {
+//         name: 'Jane',
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941
+//     },
+// ]
+// let findTheOldest = function (people) {
+//     let total = 0
+//     let OldestPerson;
+//     people.reduce((prev, curr) => {
+//         if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+//             total = (curr.yearOfDeath - curr.yearOfBirth);
+//             OldestPerson = curr
+//         }
+//         console.log(prev);
+//     })
+//     return OldestPerson
+// }
+// console.log(findTheOldest(people));
 
+// const people = [{
+//         name: 'Carly',
+//         yearOfBirth: 2018,
+//     },
+//     {
+//         name: 'Ray',
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011
+//     },
+//     {
+//         name: 'Jane',
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941
+//     },
+// ]
+
+// let findTheOldest = function (people) {
+
+//     let total = 0
+//     let OldestPerson;
+//     people.reduce((prev, curr) => {
+//         if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+//             total = (curr.yearOfDeath - curr.yearOfBirth);
+//             OldestPerson = curr
+//         }
+//         // console.log(prev);
+//     })
+//     if(!OldestPerson.hasOwnProperty('yearOfDeath')){
+//          return 'nope';
+//     }
+//     return OldestPerson;
+// }
+// console.log(findTheOldest(people));
+
+const people = [{
+        name: 'Carly',
+        yearOfBirth: 1066,
+    },
+    {
+        name: 'Ray',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011
+    },
+    {
+        name: 'Jane',
+        yearOfBirth: 1912,
+        yearOfDeath: 1941
+    },
+]
+let findTheOldest = function (people) {
+
+    let total = 0
+    let OldestPerson;
+    people.reduce((prev, curr) => {
+        if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+            total = (curr.yearOfDeath - curr.yearOfBirth)
+            OldestPerson = curr
+        }else{
+            let ind = people.filter(p => !p.hasOwnProperty('yearOfDeath') )
+            OldestPerson = ind[0]
+        }
+        // console.log(prev);
+    })
+    return OldestPerson.name;
 }
+console.log(findTheOldest(people));
 
-module.exports = findTheOldest
+// module.exports = findTheOldest
