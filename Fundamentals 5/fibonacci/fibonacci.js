376b50acab5f85706b7a089149cdc8f78ceeb3b8
safeplace12345 <safeplace12345@gmail.com>
2020-12-16 21:46:37 +0200

    Fibonacci exercvise completed

--- Fundamentals 5/fibonacci/fibonacci.js
+++ Fundamentals 5/fibonacci/fibonacci.js
@@ -1,16 +1,24 @@
 const fibonacci = function(num) {
+  if(num < 0 || num === !Number){
+    return 'OOPS'
+  }else
+  if(Number){
+    num = Number(num)
+  }
   let start,end,i,result;
-  start = 0
+  start = 1
   end = 1
-
-  for(i = 2 ; i < num ; i++){
+console.log(typeof num)
+  for(i = 1 ; i < num ; i++){
     //   console.log(i)
    result = start + end;
    start = end;
    end = result;
-   console.log(result)
   }
-  return result;
+  return start;
 }
-console.log(fibonacci(6))
-// module.exports = fibonacci
+console.log(fibonacci('8'))
+
+
+
+module.exports = fibonacci
