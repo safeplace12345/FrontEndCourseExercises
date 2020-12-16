a07f8e2b81b448defdd87720f72c64d561b2a4e7
safeplace12345 <safeplace12345@gmail.com>
2020-12-16 21:14:55 +0200

    palindromes exercise solved and get the titles

--- Fundamentals 5/getTheTitles/getTheTitles.js
+++ Fundamentals 5/getTheTitles/getTheTitles.js
@@ -1,5 +1,22 @@
-const getTheTitles = function() {
+ const books = [{
+         title: 'Book',
+         author: 'Name'
+     },
+     {
+         title: 'Book2',
+         author: 'Name2'
+     }
+ ]
 
+
+const getTheTitles = function(arg) {
+   let newArr = []
+   books.forEach(
+       (book) => {
+           newArr.push(book.title)
+       }
+   )
+   return newArr
 }
-
-module.exports = getTheTitles;
+console.log(getTheTitles(books))
+// module.exports = getTheTitles;
--- Fundamentals 5/palindromes/palindromes.js
+++ Fundamentals 5/palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    let regex = /\W/gi;
+ let lowStr = str.toLowerCase().replace(regex,'');
+ let revStr = lowStr.split('').reverse().join('');
+ return revStr === lowStr;
+ 
 }
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 
-module.exports = palindromes
+// module.exports = palindromes
